59ba551fcaa148f1c7a3d7c1766f2e78299593d2
kavenwinters <kavenwinters@arizona.edu>
2026-03-05 13:20:49 -0700

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,23 @@
+const links = Array.from(document.querySelectorAll(".item"));
+let current = 0;
+
+function setActive(i){
+  links.forEach(a => a.style.filter = "");
+  links[i].style.filter = "brightness(1.08)";
+}
+setActive(current);
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowUp"){
+    e.preventDefault();
+    current = (current - 1 + links.length) % links.length;
+    setActive(current);
+  } else if (e.key === "ArrowDown"){
+    e.preventDefault();
+    current = (current + 1) % links.length;
+    setActive(current);
+  } else if (e.key === "Enter"){
+    e.preventDefault();
+    links[current].click();
+  }
+});
